be4850cb28563f38cde6fca8247c8f222b5e1dab
Anu192 <81979262+Anu192@users.noreply.github.com>
2023-06-06 09:38:26 +0530

    Add files via upload

---
+++ Practice questions/SQUATS.py
@@ -0,0 +1,14 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+# Iterate through each test case
+for _ in range(T):
+    # Read the number of sets of squats
+    X = int(input())
+
+    # Calculate the total number of squats
+    total_squats = X * 15
+
+    # Print the result
+    print(total_squats)
