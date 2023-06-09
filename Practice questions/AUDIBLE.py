748dd6c305921c5dedacb056729dd607d05185a7
Anu192 <81979262+Anu192@users.noreply.github.com>
2023-06-09 11:52:54 +0530

    Add files via upload

---
+++ Practice questions/AUDIBLE.py
@@ -0,0 +1,13 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+# Iterate through each test case
+for _ in range(T):
+    # Read the number of sets of squats
+    X = int(input())
+    if X>=67 and X<=45000:
+        print("yes")
+    else:
+        print("NO")
+   
