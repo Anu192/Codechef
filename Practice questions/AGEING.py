4bd40ba4be37009a081f89b21018ab542c6a0843
Anu192 <81979262+Anu192@users.noreply.github.com>
2023-06-12 08:21:19 +0530

    Add files via upload

---
+++ Practice questions/AGEING.py
@@ -0,0 +1,9 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+# Iterate through each test case
+for _ in range(T):
+    # Read the number of sets of squats
+    X = int(input())
+    print(X-10)
