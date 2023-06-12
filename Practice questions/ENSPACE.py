67725e1103781baaad8972fee3fc0cfe4a1f57d1
Anu192 <81979262+Anu192@users.noreply.github.com>
2023-06-12 09:23:30 +0530

    Add files via upload

---
+++ Practice questions/ENSPACE.py
@@ -0,0 +1,10 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+# Iterate through each test case
+for _ in range(T):
+    # Read the number of sets of squats
+    X,Y,Z = map(int,input().split())
+    if (Y+Z*2)<=X:print("YES")
+    else:print("NO")
