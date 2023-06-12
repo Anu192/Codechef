d0d4c2046540e71ba3444e1fafcc7698d1a21538
Anu192 <81979262+Anu192@users.noreply.github.com>
2023-06-12 08:13:41 +0530

    Add files via upload

---
+++ Practice questions/BTRYHLTH.py
@@ -0,0 +1,11 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+# Iterate through each test case
+for _ in range(T):
+    # Read the number of sets of squats
+    X = int(input())
+    if X>=80:
+        print("YES")
+    else:print("NO")
