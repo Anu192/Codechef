1f4e35981809cb1b9193ff3f247c11739a2b9ca0
Anu192 <81979262+Anu192@users.noreply.github.com>
2023-06-12 09:30:57 +0530

    Add files via upload

---
+++ Practice questions/ONEMORE.py
@@ -0,0 +1,10 @@
+# cook your dish here
+# Read the number of test cases
+T = int(input())
+
+# Iterate through each test case
+for _ in range(T):
+    # Read the number of sets of squats
+    X = int(input())
+    if X>24:print("YES")
+    else:print("NO")
